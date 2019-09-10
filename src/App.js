f3a828379f58e4c8bfee26edc9cc29faaab40f78
joca <joaocvr@gmail.com>
2019-09-10 20:20:36 -0300

    Starts HOC subchapetr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ArchiveableList from "./scalingLocalStateInReact/highOrderComponentes/ArchiveableList";
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <div>
+        <h1>Scaling Local State In React</h1>
+        <h2>High Order Components</h2>
+        <ArchiveableList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/scalingLocalStateInReact/highOrderComponentes/ArchiveableList.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class ArchiveableList extends Component {
+  state = {
+    archivedItems: []
+  };
+
+  onArchive = id => {
+    const { archivedItems } = this.state;
+    this.setState({
+      archivedItems: [...archivedItems, id]
+    });
+  };
+
+  byArchived = archivedItems => item => !archivedItems.includes(item.id);
+
+  render() {
+    const { list } = this.props;
+    const { archivedItems } = this.state;
+    const filteredList = list.filter(() => this.byArchived(archivedItems));
+    return (
+      <ul>
+        {filteredList.map(item => (
+          <li key={item.id}>
+            <span>{item.name}</span>
+            <span>
+              <button type="button" onClick={() => this.onArchive(item.id)}>
+                Archive
+              </button>
+            </span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default ArchiveableList;
